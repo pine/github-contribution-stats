0819e37468e58bfad60b196c6c376037fee098d4
Pine Mizune <pinemz@gmail.com>
2019-06-01 04:00:17 +0900

    test: update test

--- src/test/java/moe/pine/github/contribution/stats/WebClientTest.java
+++ src/test/java/moe/pine/github/contribution/stats/WebClientTest.java
@@ -1,11 +1,14 @@
 package moe.pine.github.contribution.stats;
 
+import org.jsoup.Jsoup;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
+@PrepareForTest({WebClient.class, Jsoup.class})
 public class WebClientTest {
     private WebClient webClient;
 
