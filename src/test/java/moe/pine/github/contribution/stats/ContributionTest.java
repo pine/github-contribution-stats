2e47afc4e6323c78451b7bb39b00627e7622d505
Pine Mizune <pinemz@gmail.com>
2019-06-03 12:58:43 +0900

    test: add `equals` `hashCode` tests

--- src/test/java/moe/pine/github/contribution/stats/ContributionTest.java
+++ src/test/java/moe/pine/github/contribution/stats/ContributionTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class ContributionTest {
     @Test
@@ -14,5 +16,27 @@ public class ContributionTest {
 
         assertEquals(date, contribution.getDate());
         assertEquals(1, contribution.getCount());
+    }
+
+    @Test
+    public void equalsTest() {
+        final LocalDate date = LocalDate.of(2019, 6, 3);
+        final Contribution contribution1 = new Contribution(date, 1);
+        final Contribution contribution2 = new Contribution(date, 1);
+        final Contribution contribution3 = new Contribution(date, 2);
+
+        assertNotSame(contribution1, contribution2);
+        assertEquals(contribution1, contribution2);
+        assertNotEquals(contribution2, contribution3);
+    }
+
+    @Test
+    public void hashCodeTest() {
+        final LocalDate date = LocalDate.of(2019, 6, 3);
+        final Contribution contribution1 = new Contribution(date, 1);
+        final Contribution contribution2 = new Contribution(date, 1);
+
+        assertNotSame(contribution1, contribution2);
+        assertEquals(contribution1.hashCode(), contribution2.hashCode());
     }
 }
