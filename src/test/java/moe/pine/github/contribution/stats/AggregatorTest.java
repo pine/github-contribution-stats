a7866f7b3718a59a480da7fb45bd5e40fbaa96e1
Pine Mizune <pinemz@gmail.com>
2019-06-03 16:48:31 +0900

    test: add `Aggregator` tests

--- src/test/java/moe/pine/github/contribution/stats/AggregatorTest.java
+++ src/test/java/moe/pine/github/contribution/stats/AggregatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -23,22 +24,41 @@ public class AggregatorTest {
         final Aggregator.Streaks streaks = aggregator.getStreaks(contributions);
 
         assertEquals(
-            new Streak(6,
-                LocalDate.of(2019, 5, 27),
-                LocalDate.of(2019, 6, 1),
-                false),
-            streaks.currentStreak);
+                new Streak(6,
+                        LocalDate.of(2019, 5, 27),
+                        LocalDate.of(2019, 6, 1),
+                        false),
+                streaks.currentStreak);
 
         assertEquals(
-            new Streak(77,
-                LocalDate.of(2018, 5, 27),
-                LocalDate.of(2018, 8, 11),
-                false),
-            streaks.longestStreak);
+                new Streak(77,
+                        LocalDate.of(2018, 5, 27),
+                        LocalDate.of(2018, 8, 11),
+                        false),
+                streaks.longestStreak);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void getStreaksTest_illegalContributions() {
         aggregator.getStreaks(Collections.emptyList());
+    }
+
+    @Test
+    public void summarizeContributionsTest() {
+        final List<Contribution> contributions = Arrays.asList(
+                new Contribution(LocalDate.of(2019, 5, 31), 0),
+                new Contribution(LocalDate.of(2019, 6, 1), 1),
+                new Contribution(LocalDate.of(2019, 6, 2), 2),
+                new Contribution(LocalDate.of(2019, 6, 3), 3)
+        );
+
+        final Summary summary = aggregator.summarizeContributions(contributions);
+
+        assertEquals(LocalDate.of(2019, 5, 31), summary.getStart());
+        assertEquals(LocalDate.of(2019, 6, 3), summary.getEnd());
+        assertEquals(6, summary.getTotal());
+        assertEquals(
+                new Contribution(LocalDate.of(2019, 6, 3), 3),
+                summary.getBusiestDay());
     }
 }
